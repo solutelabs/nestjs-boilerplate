07ca23d7f15eff9b0a8196676e0cec8ef6770e48
sambhav <sambhav.c@solutelabs.com>
2021-03-22 17:57:27 +0530

    feat(aws): added delete assets and multi signed url service

--- src/utility/s3/models/index.ts
+++ src/utility/s3/models/index.ts
@@ -1,1 +1,2 @@
+export * from './multis-signed-url.model';
 export * from './signed-url.model';
--- src/utility/s3/s3.resolver.ts
+++ src/utility/s3/s3.resolver.ts
@@ -1,7 +1,7 @@
 import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { S3Service } from './s3.service';
-import { S3Dto } from './dto';
-import { SignedURL } from './models';
+import { MultiS3Dto, S3Dto } from './dto';
+import { MutiSignedURL, SignedURL } from './models';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../guards';
 
@@ -17,5 +17,14 @@ export class S3Resolver {
   ) {
     const resp = await this.s3Service.getPresignedURL(s3Dto.fileName);
     return { url: resp };
+  }
+
+  @Mutation(() => [MutiSignedURL])
+  @UseGuards(JwtAuthGuard)
+  multiSignedURL(
+    @Args('input', { type: () => MultiS3Dto })
+    multiS3Dto: MultiS3Dto,
+  ) {
+    return this.s3Service.getMultiPresignedURL(multiS3Dto.fileNames);
   }
 }
